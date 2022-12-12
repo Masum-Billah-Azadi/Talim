4d80d5958d6ff10d2f7a542ee8e960fc62c31809
Masum-Billah-Azadi <107561351+Masum-Billah-Azadi@users.noreply.github.com>
2022-12-12 22:29:29 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -17,4 +17,4 @@ export const auth = getAuth();
 
 export default app;
 
-Talim is a knowledge base social media application is a type of online platform that allows users to share and access information about a specific topic or set of topics. This type of application is typically used by communities of experts or enthusiasts who want to share their knowledge and learn from each other.
+T
