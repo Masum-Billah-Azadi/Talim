83febc736e459b789aa3cb8912cc01fb1ad6472b
Masum-Billah-Azadi <mdmasumbillahajadi1@gmail.com>
2022-12-25 12:04:31 +0600

    get user post from firestor

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,39 +1,66 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import {
+  addDoc,
+  collection, onSnapshot,
+  orderBy, query
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../../firebase";
 
-const Home = () => {
-    const [nweet, setNweet] = useState("");
-    const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-    const docRef = await addDoc(collection(dbService, "nweets" ), {
-    nweet,
-    createdAt: Date.now(),
-    });
-    console.log(" Document written with ID: ", docRef.id);
-    } catch (error) {
-    console.error(" Error adding document: ", error);
-    }
-    
-    setNweet("");
-    };
-    const onChange = ({ target: { value } })=>{
-    setNweet(value);
-    };
-    
-    return (
+const Home = ({ userObj }) => {
+const [nweet, setNweet] = useState("");
+const [nweets, setNweets] = useState([]);
+
+useEffect(() => {
+const q = query(
+collection(dbService, "nweets"),
+orderBy("createdAt", "desc")
+);
+onSnapshot(q, (snapshot) => {
+const nweetArr = snapshot.docs.map((document) =>({
+id: document.id,
+...document.data(),
+}));
+setNweets(nweetArr);
+});
+}, []);
+
+const onSubmit = async (e) => {
+  e.preventDefault();
+  try {
+  const docRef = await addDoc(collection(dbService, "nweets" ), {
+  nweet,
+  createdAt: Date.now(),
+  });
+  console.log(" Document written with ID: ", docRef.id);
+  } catch (error) {
+  console.error(" Error adding document: ", error);
+  }
+  
+  setNweet("");
+  };
+  const onChange = ({ target: { value } })=>{
+  setNweet(value);
+  };
+return (
         <div>
           <form onSubmit={onSubmit}>
             <input
               value={nweet}
               onChange={onChange}
               type="text"
-              placeholder="What's on your mind?"
+              placeholder=" What's on your mind"
               maxLength={120}
-            />
+              />
             <input type="submit" value="Nweet" />
           </form>
+          <div>
+            {nweets.map((nweet) => (
+              <div key={nweet.id}>
+                <h4>{nweet.nweet}</h4>
+                <h4>{nweet.text}</h4>
+              </div>
+            ))}
+          </div>
         </div>
       );
     }
---
+++ src/components/pages/example.js
@@ -0,0 +1,150 @@
+import { initializeApp } from 'firebase/app'
+import {
+    createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut
+} from 'firebase/auth'
+import {
+    addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, serverTimestamp,
+    updateDoc, where
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmXgb_58lO7aK_ujN37pGlNxzWGEU0YpI",
+  authDomain: "fb9-sandbox.firebaseapp.com",
+  projectId: "fb9-sandbox",
+  storageBucket: "fb9-sandbox.appspot.com",
+  messagingSenderId: "867529587246",
+  appId: "1:867529587246:web:dc754ab7840c737f47bdbf"
+}
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+const auth = getAuth()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// queries
+const q = query(colRef, where("author", "==", "patrick rothfuss"), orderBy('createdAt'))
+
+// realtime collection data
+const unsubCol = onSnapshot(q, (snapshot) => {
+  let books = []
+  snapshot.docs.forEach(doc => {
+    books.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(books)
+})
+
+// adding docs
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+    createdAt: serverTimestamp()
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
+
+// fetching a single document (& realtime)
+const docRef = doc(db, 'books', 'gGu4P9x0ZHK9SspA1d9j')
+
+const unsubDoc = onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id)
+})
+
+// updating a document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let docRef = doc(db, 'books', updateForm.id.value)
+
+  updateDoc(docRef, {
+    title: 'updated title'
+  })
+  .then(() => {
+    updateForm.reset()
+  })
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user created:', cred.user)
+      signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// subscribing to auth changes
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+  console.log('user status changed:', user)
+})
+
+// unsubscribing from changes (auth & db)
+const unsubButton = document.querySelector('.unsub')
+unsubButton.addEventListener('click', () => {
+  console.log('unsubscribing')
+  unsubCol()
+  unsubDoc()
+  unsubAuth()
+})
