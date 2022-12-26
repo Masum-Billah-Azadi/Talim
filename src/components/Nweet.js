146db812e1aafb7ea3f4c38c551b9f6b45b36da2
Masum-Billah-Azadi <mdmasumbillahajadi1@gmail.com>
2022-12-26 12:51:15 +0600

    fireBase 9 edite and delet add

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,3 +1,4 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { dbService } from "../firebase";
 
@@ -7,47 +8,49 @@ const Nweet = ({ nweetObj, isOwner }) => {
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
-      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      // delete nweet
+      await deleteDoc(doc(dbService, `nweets/${nweetObj.id}`));
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await dbService.doc(`nweets/${nweetObj.id}`).update({
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await updateDoc(doc(dbService, `nweets/${nweetObj.id}`), {
       text: newNweet,
     });
     setEditing(false);
   };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
+  const onChange = ({ target: { value } }) => {
     setNewNweet(value);
   };
   return (
-    <div>
+    <div className="nweet">
       {editing ? (
         <>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={onSubmit} className="container nweetEdit">
             <input
               type="text"
-              placeholder="Edit your nweet"
+              placeholder="Edit your Nweet"
               value={newNweet}
               required
               onChange={onChange}
+              autoFocus
+              className="formInput"
             />
-            <input type="submit" value="Update Nweet" />
+            <input type="submit" value="Update Nweet"/>
           </form>
-          <button onClick={toggleEditing}>Cancel</button>
+          <span onClick={toggleEditing}>
+            Cancel
+          </span>
         </>
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
           {isOwner && (
             <>
-              <button onClick={onDeleteClick}>Delete Nweet</button>
-              <button onClick={toggleEditing}>Edit Nweet</button>
-            </>
+            <button onClick={onDeleteClick}>Delete Nweet</button>
+            <button onClick={toggleEditing}>Edit Nweet</button>
+          </>
           )}
         </>
       )}
