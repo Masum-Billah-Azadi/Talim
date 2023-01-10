c36bb3ddbde2ac631c1f789a8a82dd859e5780bc
Masum-Billah-Azadi <mdmasumbillahajadi1@gmail.com>
2023-01-11 02:20:53 +0600

    Navigation Style Done

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -46,10 +46,6 @@ const AppRouter = ()=>{
     return(
       <div 
       style={{
-        maxWidth: 890,
-        width: "100%",
-        margin: "0 auto",
-        marginTop: 80,
         display: "flex",
         justifyContent: "center",
       }}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,47 +1,92 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faBell, faEnvelope, faHashtag, faHome, faList, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from "./Styles/Nav.module.css";
 
 const Navigation = ({userObj}) => {
   
   return(
-  <>
-    <nav className={classes.nav}>
+  <div className="Navigation">
+    <nav>
       <ul>
-        <li>
-          <Link to="/" style={{ marginRight: 10 }}>
-          <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+          <Link to="/">
+          <div id="homeIcon">
+            <FontAwesomeIcon icon={faTwitter} color={"#ffff"} size="2x" />
+          </div>
+          </Link>
+        <li style={{ marginTop:30 }}>
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faHome} color={"#ffff"} size="2x" />
+          </div>
+          <span>Home</span>
           </Link>
         </li>
         <li>
-        <Link
-            to="/profile"
-            style={{
-              marginLeft: 10,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              fontSize: 12,
-            }}
-          > 
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faHashtag} color={"#ffff"} size="2x" />
+          </div>
+          <span>Explore</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faBell} color={"#ffff"} size="2x" />
+          </div>
+          <span>Notifications</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faEnvelope} color={"#ffff"} size="2x" />
+          </div>
+          <span>Messages</span>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faUser} color={"#ffff"} size="2x" />
+          </div>
+          <span>Profile</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+          <div className="NavIcon">
+            <FontAwesomeIcon icon={faList} color={"#ffff"} size="2x" />
+          </div>
+          <span>More</span>
+          </Link>
+        </li>
+        <div className="TopTweet">
+          <Link to="/">
+          <span>Tweet</span>
+          </Link>
+        </div>
+      </ul>
+      <Link to="/profile"> 
+          <div className="NaveBotttom">
             <span>
               {userObj.photoURL
                 ? <img className="ProfilePicture" src={userObj.photoURL} />
-                : <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />}
+                : <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />}
             </span>
-            <span style={{ marginTop: 10 }}>
+            <h1>
               {userObj.displayName
                 ? `${userObj.displayName} Profile`
                 : "Profile"}
-            </span>
-          </Link>
-        </li>
-      </ul>
+            </h1>
+          </div>
+        </Link>
     </nav>
-  </>
+  </div>
 )
 };
 export default Navigation;
