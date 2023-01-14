60fdf02b956e9332f6d36afba6ec72b303e772dd
Masum-Billah-Azadi <mdmasumbillahajadi1@gmail.com>
2023-01-15 02:42:07 +0600

    Right Site Style Complite

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-no-comment-textnodes */
 
-import { faHandSparkles } from '@fortawesome/free-solid-svg-icons';
+import { faHandSparkles, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
@@ -48,7 +48,42 @@ return (
           </>
         </div>
         <div className='Right_Side'>
-          <h1>Masum</h1>
+          <div className='Search_p'>
+            <div className='Searchbar'>
+              <FontAwesomeIcon icon={faSearch} color={"#1c9aef"} size="1x" />
+              <input
+                type="text"
+                placeholder='Search Twitter'
+              />
+            </div>
+          </div>
+          
+          <div className='TrandTropic'>
+            <h1>Trends for you</h1>
+              <div className='TTropic'>
+                <p>Politic<span>Trending</span></p>
+                <h3>Taliban</h3>
+                <p>29k Tweets</p>
+              </div>
+              <div className='TTropic'>
+                <p>Bangladesh<span>Trending</span></p>
+                <h3>ChatGpt</h3>
+                <p>89k Tweets</p>
+              </div>
+          </div>
+          <div className='WTFollow'>
+              <h1>Who to follow</h1>
+              <div className='FollowParson'>
+              {userObj.photoURL
+                ? <img className="ProfilePicture" src={userObj.photoURL} />
+                : <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />}
+                <div className='Parson_name'>
+                  <h4>{userObj.displayName}</h4>
+                  <p>@{userObj.displayName}</p>
+                </div>
+                <button>Follow</button>
+              </div>
+          </div>
         </div>
       </div>
       );
